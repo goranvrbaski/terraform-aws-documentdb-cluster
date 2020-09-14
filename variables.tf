48d598c146137fcc08711661cfc3cb37bbf9b54b
Goran Vrbaški <vrbaski.goran@gmail.com>
2020-09-14 12:39:08 +0200

    fix variable scoping

--- variables.tf
+++ variables.tf
@@ -1,45 +1,45 @@
 variable "cluster_name" {
-  type = string
+  type        = string
   description = "Name of the cluster to create"
 }
 
 variable "master_username" {
-  type = string
+  type        = string
   description = "Username to use for master user"
 }
 
 variable "backup_window" {
-  type    = string
-  default = "07:00-09:00"
+  type        = string
+  default     = "07:00-09:00"
   description = "Backup window for the cluster"
 }
 
 variable "maintenance_window" {
-  type    = string
-  default = "Mon:00:00-Mon:03:00"
+  type        = string
+  default     = "Mon:00:00-Mon:03:00"
   description = "Maintenance window for the cluster"
 }
 
 variable "backup_rentetion_period" {
-  type    = number
-  default = 7
+  type        = number
+  default     = 7
   description = "Number of automated backups to keep"
 }
 
 variable "skip_final_snapshot" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
   description = "Skip final snapshot on the cluster delete"
 }
 
 variable "instance_count" {
-  type    = number
-  default = 2
+  type        = number
+  default     = 2
   description = "Instance number to create in the cluster"
 }
 variable "instance_class" {
-  type    = string
-  default = "db.t3.medium"
+  type        = string
+  default     = "db.t3.medium"
   description = "Instance type to use when creating the cluster"
 }
 
@@ -52,30 +52,30 @@ variable "tags" {
 }
 
 variable "availability_zones" {
-  type = list(string)
+  type        = list(string)
   description = "Availability zones for the cluster instances"
 }
 
 variable "storage_encrypted" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
   description = "Encryption at rest"
 }
 
 variable "subnet_group_ids" {
-  type    = list(string)
-  default = []
+  type        = list(string)
+  default     = []
   description = "List of subnet group ids to use when provisioning instances"
 }
 
 variable "security_group_ids" {
-  type    = list(string)
-  default = []
-  description "List of security group ids to attach to the cluster"
+  type        = list(string)
+  default     = []
+  description = "List of security group ids to attach to the cluster"
 }
 
 variable "password_length" {
-  type    = number
-  default = 16
+  type        = number
+  default     = 16
   description = "Length of master password"
 }
