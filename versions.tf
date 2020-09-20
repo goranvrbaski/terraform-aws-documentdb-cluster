3dec2907ca8ca7b0b199f32bb54fbd82a1aca775
Goran Vrbaški <vrbaski.goran@gmail.com>
2020-09-20 20:44:37 +0200

    remove random provider from versions.tf

--- versions.tf
+++ versions.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.6.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 2.3.0"
-    }
   }
   required_version = ">= 0.13"
 }
